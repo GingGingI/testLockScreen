0cbe3eca453e37f3a97a269bb91bf295caca49fc
GingGingI <sma0147@naver.com>
2019-05-26 16:26:20 +0900

    Support notch Layout stack fixed

--- app/src/main/java/com/example/testlockscreen/lockScreen/testLockScreen.kt
+++ app/src/main/java/com/example/testlockscreen/lockScreen/testLockScreen.kt
@@ -3,6 +3,8 @@ package com.example.testlockscreen.lockScreen
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +16,7 @@ class testLockScreen: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         flagInit()
         setContentView(R.layout.lock_screen)
-
+        Log.i("hi","start")
         setClock()
     }
 
@@ -22,12 +24,25 @@ class testLockScreen: AppCompatActivity() {
 
     }
 
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+
     @SuppressLint("NewApi")
     private fun flagInit() {
+
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.run {
-            setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+
+            setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_ATTACHED_IN_DECOR,
+                WindowManager.LayoutParams.FLAG_LAYOUT_ATTACHED_IN_DECOR)
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) setShowWhenLocked(true)
             else addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED)
